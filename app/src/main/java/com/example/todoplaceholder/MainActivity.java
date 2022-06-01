2512fab64a3ad8cd4323b619503d106a73f16c39
xNikus <comlokoxd@gmail.com>
2022-06-01 22:33:48 +0200

    Navbar Decoration

--- app/src/main/java/com/example/todoplaceholder/MainActivity.java
+++ app/src/main/java/com/example/todoplaceholder/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todoplaceholder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
     }
 }
