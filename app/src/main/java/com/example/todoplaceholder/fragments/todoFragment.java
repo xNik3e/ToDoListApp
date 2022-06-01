2a2a871715ea73d4f27584d607919f9d4e81b8df
xNikus <comlokoxd@gmail.com>
2022-06-02 00:53:13 +0200

    Todo Window RecyclerView for Calendar Card

---
+++ app/src/main/java/com/example/todoplaceholder/fragments/todoFragment.java
@@ -0,0 +1,36 @@
+package com.example.todoplaceholder.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.todoplaceholder.R;
+
+public class todoFragment extends Fragment {
+
+
+    private Context context;
+
+    public todoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo, container, false);
+    }
+}
