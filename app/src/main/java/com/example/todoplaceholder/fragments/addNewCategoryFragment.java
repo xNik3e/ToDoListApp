bbfbc4d3c02ebe9e7bb60eeefb36bc21fb8101c6
xNikus <comlokoxd@gmail.com>
2022-06-02 04:59:16 +0200

    Add new category window

---
+++ app/src/main/java/com/example/todoplaceholder/fragments/addNewCategoryFragment.java
@@ -0,0 +1,37 @@
+package com.example.todoplaceholder.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.todoplaceholder.R;
+
+
+public class addNewCategoryFragment extends Fragment {
+
+    private Context context;
+
+    public addNewCategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_new_category, container, false);
+    }
+}
