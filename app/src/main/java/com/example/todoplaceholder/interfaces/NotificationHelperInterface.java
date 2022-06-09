ef474e0b7ffa163bee51500dfafaae6d4a85b01b
xNikus <comlokoxd@gmail.com>
2022-06-09 21:28:57 +0200

    Create NotificationHelperInterface.java

---
+++ app/src/main/java/com/example/todoplaceholder/interfaces/NotificationHelperInterface.java
@@ -0,0 +1,5 @@
+package com.example.todoplaceholder.interfaces;
+
+public interface NotificationHelperInterface {
+    public void notifyMe(String title, String description, int notificationID, long notifTime);
+}
